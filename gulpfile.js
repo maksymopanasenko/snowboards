e5d22ef4ff93245ed0f86b97e762a61a3e851e7c
Max <maxpanas008@gmail.com>
2023-02-01 14:05:37 +0100

    files are moved to dist folder. Ready to deploy

--- gulpfile.js
+++ gulpfile.js
@@ -6,19 +6,20 @@ const cleanCSS = require('gulp-clean-css');
 const rename = require("gulp-rename");
 const imagemin = require('gulp-imagemin');
 const htmlmin = require('gulp-htmlmin');
-
+ 
 
 
 gulp.task('server', function() {
     browserSync.init({
         server: {
-            baseDir: "src"
+            baseDir: "dist"
         }
         // proxy: "theage"
     });
 
     gulp.watch('src/*.html').on('change', browserSync.reload);
 });
+
 
 
 gulp.task('styles', function() {
@@ -33,6 +34,7 @@ gulp.task('styles', function() {
         }))
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest('src/css'))
+        .pipe(gulp.dest('dist/css'))
         .pipe(browserSync.stream());
 });
 
@@ -40,17 +42,37 @@ gulp.task('watch', function() {
     gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
     gulp.watch('src/css/*.css').on('change', browserSync.reload);
     gulp.watch('src/js/*.js').on('change', browserSync.reload);
+    gulp.watch('src/*.html').on('change', browserSync.reload);
 });
 
-// gulp.task('html', function() {
-//     return gulp.src('src/*html')
-//         .pipe(htmlmin({collapseWhitespace: true}))
-//         .pipe(gulp.dest("dist/"));
-// });
+gulp.task('html', function() {
+    return gulp.src('src/*html')
+        // .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest("src/"))
+        .pipe(gulp.dest('dist/'));
+});
 
 gulp.task('images', function() {
     return gulp.src('src/img/**/*')
-        .pipe(gulp.dest('src/img'));
+        .pipe(gulp.dest('src/img'))
+        .pipe(gulp.dest('dist/img'));
 });
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'images'));
+gulp.task('icons', function() {
+    return gulp.src('src/icons/**/*')
+        .pipe(gulp.dest('src/icons'))
+        .pipe(gulp.dest('dist/icons'));
+});
+
+gulp.task('js', function() {
+    return gulp.src('src/js/**/*.js')
+        .pipe(gulp.dest('src/js'))
+        .pipe(gulp.dest('dist/js'));
+});
+
+// gulp.task('db', function() {
+//     return gulp.src('src/*json')
+//         .pipe(gulp.dest('src/icons'));
+// });
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'images', 'icons', 'html', 'js'));
