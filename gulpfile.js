bdb18bba800dd6862eebe0a8e630760abab31e80
Max <maxpanas008@gmail.com>
2023-07-12 21:23:55 +0200

    modernize gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,32 @@
 const gulp = require('gulp');
-const browserSync = require('browser-sync');
+const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
 const rename = require("gulp-rename");
 const imagemin = require('gulp-imagemin');
 const htmlmin = require('gulp-htmlmin');
+const clean = require('gulp-clean');
  
 
 
-gulp.task('server', function() {
+const dev = () => {
     browserSync.init({
         server: {
-            baseDir: "src"
+            baseDir: "./dist"
         }
-        // proxy: "theage"
     });
 
-    gulp.watch('src/*.html').on('change', browserSync.reload);
-});
+    gulp.watch('./src/**/*', gulp.series('cleanDist', gulp.parallel('styles', 'images', 'icons', 'html', 'js', 'json'), (next) => {
+        browserSync.reload()
+        next();
+    }))
+}
 
-
+gulp.task('cleanDist', function() {
+    return gulp.src('./dist', {read: false})
+    .pipe(clean());
+})
 
 gulp.task('styles', function() {
     return gulp.src('src/sass/*.+(scss|sass)')
@@ -33,47 +39,33 @@ gulp.task('styles', function() {
             cascade: false
         }))
         .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest('src/css'))
-        .pipe(gulp.dest('dist/css'))
-        .pipe(browserSync.stream());
-});
-
-gulp.task('watch', function() {
-    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
-    gulp.watch('src/css/*.css').on('change', browserSync.reload);
-    gulp.watch('src/js/*.js').on('change', browserSync.reload);
-    gulp.watch('src/*.html').on('change', browserSync.reload);
+        .pipe(gulp.dest('dist/css'));
 });
 
 gulp.task('html', function() {
     return gulp.src('src/*html')
-        // .pipe(htmlmin({collapseWhitespace: true}))
-        .pipe(gulp.dest("src/"))
+        .pipe(htmlmin({collapseWhitespace: true}))
         .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('images', function() {
     return gulp.src('src/img/**/*')
-        .pipe(gulp.dest('src/img'))
         .pipe(gulp.dest('dist/img'));
 });
 
 gulp.task('icons', function() {
     return gulp.src('src/icons/**/*')
-        .pipe(gulp.dest('src/icons'))
         .pipe(gulp.dest('dist/icons'));
 });
 
 gulp.task('js', function() {
     return gulp.src('src/js/**/*.js')
-        .pipe(gulp.dest('src/js'))
         .pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('json', function() {
-    return gulp.src('src/*json')
-        .pipe(gulp.dest('src/'))
+    return gulp.src('src/*.json')
         .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'images', 'icons', 'html', 'js', 'json'));
+gulp.task('default', gulp.series(gulp.parallel('styles', 'images', 'icons', 'html', 'js', 'json'), dev));
